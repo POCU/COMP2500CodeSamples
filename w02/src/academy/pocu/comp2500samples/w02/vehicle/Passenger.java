6305e1af0df8cd413b83f1a0f711d50129c077fc
monkeywithbanana <>
2020-02-02 21:30:14 -0800

    week 2 WIP

---
+++ w02/src/academy/pocu/comp2500samples/w02/vehicle/Passenger.java
@@ -0,0 +1,15 @@
+package academy.pocu.comp2500samples.w02.vehicle;
+
+public class Passenger {
+    public String Name;
+
+    public Passenger(String name) {
+        this.Name = name;
+    }
+
+    public void sayName() {
+        System.out.println(String.format("My name is %s.", this.Name));
+    }
+
+
+}
