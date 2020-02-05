41498878c2ed824d6e8ff95b6cd14ae8bcfaf0b9
monkeywithbanana <>
2020-02-04 21:44:05 -0800

    week03 initial commit

---
+++ 03/src/academy/pocu/comp2500samples/w03/privateaccessmodifier/Program.java
@@ -0,0 +1,7 @@
+package academy.pocu.comp2500samples.w03.privateaccessmodifier;
+
+public class Program {
+    public static void main(String[] args) {
+        System.out.println("Hello");
+    }
+}
